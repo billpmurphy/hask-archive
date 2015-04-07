25d93fe3dd866640eae1e6446cf72e650fc87c77
billpmurphy <billpmurphy92@gmail.com>
2015-04-07 17:55:17 -0400

    tests for Haskell type signature parsing

--- tests.py
+++ tests.py
@@ -6,6 +6,11 @@ from pythaskell.syntax import NoGuardMatchException as NGME
 from pythaskell.types import Just
 from pythaskell.types import Nothing
 
+from pythaskell.static import parse_haskell_typestring
+from pythaskell.static import parse_constraints
+from pythaskell.static import parse_signature
+from pythaskell.static import check_paren_balance
+from pythaskell.static import TypeStringException as TSE
 
 class TestSyntax(unittest.TestCase):
 
@@ -34,6 +39,94 @@ class TestTypes(unittest.TestCase):
 
 class TestHOF(unittest.TestCase):
     pass
+
+
+
+###
+
+class TestParse(unittest.TestCase):
+
+    def setUp(self):
+        # some aliases for readability
+        self.psig = parse_signature
+        self.assertNumArgs = lambda n,x: self.assertEqual(n, len(self.psig(x)))
+
+        self.pcon = parse_constraints
+        self.assertCons = lambda n,x: self.assertEqual(n, len(self.pcon(x)))
+
+        self.parse = parse_haskell_typestring
+        self.assertParseArgs = lambda n,x: self.assertEqual(n,len(self.parse(x)))
+
+
+    def test_check_parens(self):
+        self.assertTrue(check_paren_balance(""))
+        self.assertTrue(check_paren_balance("()"))
+        self.assertTrue(check_paren_balance("(())"))
+        self.assertTrue(check_paren_balance("((()))(())"))
+        self.assertTrue(check_paren_balance("()()()"))
+        self.assertTrue(check_paren_balance("(()())()"))
+        self.assertTrue(check_paren_balance("((()())(()(()))())"))
+
+        self.assertFalse(check_paren_balance("("))
+        self.assertFalse(check_paren_balance(")"))
+
+    def test_parse_constraint(self):
+        self.assertCons(1, "Monad m")
+        self.assertCons(2, "Monad m,Functor f")
+        self.assertCons(2, "Monad m, Functor f")
+        self.assertCons(3, "Monad m,Functor f,Num a")
+        self.assertCons(3, "Monad m, Functor f, Num a")
+        self.assertCons(3, "Monad m, Functor m, Num a")
+
+        with self.assertRaises(TSE): self.pcon("Monadm")
+        with self.assertRaises(TSE): self.pcon("Monad m a")
+        with self.assertRaises(TSE): self.pcon("Monad m a, Functor f")
+
+    def test_parse_sig(self):
+        self.assertNumArgs(1, "a")
+        self.assertNumArgs(1, "m a")
+
+        self.assertNumArgs(2, "a -> a")
+        self.assertNumArgs(2, "a -> b")
+        self.assertNumArgs(2, "a -> m a")
+        self.assertNumArgs(2, "g a -> a")
+        self.assertNumArgs(2, "g t b a -> m f a")
+        self.assertNumArgs(2, "g -> (t -> b -> a -> m -> f a)")
+        self.assertNumArgs(2, "g -> ((t -> b -> a) -> (m -> f a))")
+        self.assertNumArgs(2, "g -> ((t -> (b -> a)) -> (m -> f a))")
+
+        self.assertNumArgs(3, "f :: a -> b -> b")
+        self.assertNumArgs(3, "f :: (a -> b) -> a -> b")
+        self.assertNumArgs(3, "f :: a -> (a -> b) -> b")
+        self.assertNumArgs(3, "f :: a -> b -> (a -> b)")
+        self.assertNumArgs(3, "g -> (t -> b -> a) -> (m -> f a)")
+
+        self.assertNumArgs(4, "f :: a -> b -> c -> a")
+        self.assertNumArgs(5, "f :: a -> b -> c -> d -> a")
+
+    def test_whitespace(self):
+        with self.assertRaises(TSE): self.parse(" f :: a -> a")
+        with self.assertRaises(TSE): self.parse("f :: a -> a ")
+        with self.assertRaises(TSE): self.parse("f  :: a -> a")
+        with self.assertRaises(TSE): self.parse("f:: a -> a")
+        with self.assertRaises(TSE): self.parse("f ::a -> a")
+        with self.assertRaises(TSE): self.parse("f ::a -> a")
+        with self.assertRaises(TSE): self.parse("f :: a-> b")
+        with self.assertRaises(TSE): self.parse("f :: a ->b")
+        with self.assertRaises(TSE): self.parse("f :: a->b")
+
+    def test_structure(self):
+        with self.assertRaises(TSE): self.parse("asdf")
+        with self.assertRaises(TSE): self.parse("f ::")
+        with self.assertRaises(TSE): self.parse("f-f ::")
+        with self.assertRaises(TSE): self.parse("f f ::")
+        with self.assertRaises(TSE): self.parse("f :: a => a")
+        with self.assertRaises(TSE): self.parse("f :: (Num a) => a => a")
+
+    def test_full_parse(self):
+        self.assertParseArgs(1, "f :: a")
+        self.assertParseArgs(1, "f :: (Monad m) => m a")
+        self.assertParseArgs(1, "f :: (Monad m, Functor f) => m f a")
 
 
 if __name__ == '__main__':
